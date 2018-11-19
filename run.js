a214f3783725890a057e845431c51754c79a4f5c
Johannes Brottrager <e1326320@student.tuwien.ac.at>
2018-11-19 13:05:56 +0100

    Initial commit inkl first work

---
+++ run.js
@@ -0,0 +1,12 @@
+var MetaCoin = artifacts.require("./contracts/MetaCoin.sol");
+
+module.exports = function(callback) {
+
+    MetaCoin.deployed().then(function(instance) {
+        instance.mint("0xF4CbDa98d2ea054FB0fcB452646C4E973674D209",100);
+        instance.getBalance("0xF4CbDa98d2ea054FB0fcB452646C4E973674D209").then(function(balance){
+            console.log("balance: "+balance);
+        });
+    });
+}
+
