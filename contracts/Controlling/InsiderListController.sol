fe699a5c056955169fc0103bbc8840246125aff9
Johannes Brottrager <e1326320@student.tuwien.ac.at>
2019-05-09 12:42:39 +0200

    Implement Insider Lists

--- contracts/Controlling/InsiderListController.sol
+++ contracts/Controlling/InsiderListController.sol
@@ -3,18 +3,19 @@ pragma solidity ^0.5.4;
 import "../Interfaces/IController.sol";
 import "../Roles/KYCVerifierRole.sol";
 
-//Global controller that stores addresses in a whitelist, TODO enter into registry
-//Allows all actions for all whitelisted addresses (sender and recipient, if applicable)
-//TODO include some kind of link to the decentralized information
+//Controller that stores addresses in a blacklist, corresponds to a single token (one company normally)
+//Allows no actions for all blacklisted addresses (sender and recipient, if applicable)
+//may be overwritten if the controlling entity signs a transaction
+// This contract in combination with the stored customer information from the decentralized data storage of customers
+// constitutes an insider list according to https://eur-lex.europa.eu/legal-content/DE/TXT/PDF/?uri=CELEX:32016R0347&from=DE
 contract KYCController is IController, KYCVerifierRole {
     byte internal constant STATUS_SUCCESS = 0x51; // Uses status codes from ERC-1066
     byte internal constant STATUS_FAIL = 0x50;
 
-    event AddedToWhitelist(address added);
-    event RemovedFromWhitelist(address added);
+    event AddedToBlacklist(address added);
+    event RemovedFromBlacklist(address added);
 
-    mapping (address => bool) public whitelist; //TODO eventually store some some struct on what type of kyc is stored
-
+    mapping (address => bool) public blacklist;
 
     /**
     * @notice Verify if an issuance to an address is allowed
@@ -27,13 +28,13 @@ contract KYCController is IController, KYCVerifierRole {
     function verifyIssue(address _tokenHolder, uint256 _value, bytes calldata _data) external view
     returns (bool allowed, byte statusCode)
     {
-        if(_onWhitelist(_tokenHolder)){
-            allowed = true;
-            statusCode = STATUS_SUCCESS;
-        }
-        else{
+        if(_onBlacklist(_tokenHolder)){
             allowed = false;
             statusCode = STATUS_FAIL;
+        }
+        else{
+            allowed = true;
+            statusCode = STATUS_SUCCESS;
         }
     }
 
@@ -48,13 +49,13 @@ contract KYCController is IController, KYCVerifierRole {
     function verifyTransfer(address _from, address _to, uint256 _amount, bytes calldata _data) external view
     returns (bool allowed, byte statusCode)
     {
-        if(_onWhitelist(_from) && _onWhitelist(_to)){ //TODO application specific more detailed Status codes?
-            allowed = true;
-            statusCode = STATUS_SUCCESS;
-        }
-        else{
+        if(_onBlacklist(_from) && _onBlacklist(_to)){ //TODO application specific more detailed Status codes?
             allowed = false;
             statusCode = STATUS_FAIL;
+        }
+        else{
+            allowed = true;
+            statusCode = STATUS_SUCCESS;
         }
     }
 
@@ -69,13 +70,13 @@ contract KYCController is IController, KYCVerifierRole {
     function verifyTransferFrom(address _from, address _to, address _forwarder, uint256 _amount, bytes calldata _data) external view
     returns (bool allowed, byte statusCode)
     {
-        if(_onWhitelist(_from) && _onWhitelist(_to) && _onWhitelist(_forwarder)){
-            allowed = true;
-            statusCode = STATUS_SUCCESS;
-        }
-        else{
+        if(_onBlacklist(_from) && _onBlacklist(_to) && _onBlacklist(_forwarder)){
             allowed = false;
             statusCode = STATUS_FAIL;
+        }
+        else{
+            allowed = true;
+            statusCode = STATUS_SUCCESS;
         }
     }
 
@@ -90,13 +91,13 @@ contract KYCController is IController, KYCVerifierRole {
     function verifyRedeem(address _sender, uint256 _amount, bytes calldata _data) external view
     returns (bool allowed, byte statusCode)
     {
-        if(_onWhitelist(_sender)){
-            allowed = true;
-            statusCode = STATUS_SUCCESS;
-        }
-        else{
+        if(_onBlacklist(_sender)){
             allowed = false;
             statusCode = STATUS_FAIL;
+        }
+        else{
+            allowed = true;
+            statusCode = STATUS_SUCCESS;
         }
     }
 
@@ -111,13 +112,13 @@ contract KYCController is IController, KYCVerifierRole {
     function verifyRedeemFrom(address _sender, address _tokenHolder, uint256 _amount, bytes calldata _data) external view
     returns (bool allowed, byte statusCode)
     {
-        if(_onWhitelist(_sender) && _onWhitelist(_tokenHolder)){
-            allowed = true;
-            statusCode = STATUS_SUCCESS;
-        }
-        else{
+        if(_onBlacklist(_sender) && _onBlacklist(_tokenHolder)){
             allowed = false;
             statusCode = STATUS_FAIL;
+        }
+        else{
+            allowed = true;
+            statusCode = STATUS_SUCCESS;
         }
     }
 
@@ -126,8 +127,8 @@ contract KYCController is IController, KYCVerifierRole {
     * @dev Only addresses with the role @KYCVerifier are allowed to use this Function
     */
     function addAddressToWhitelist(address _addr) external onlyKYCVerifier { //TODO do I have to set my instance of KYCVerifier somewhere central, or are they linked by deploying?
-        whitelist[_addr] = true;
-        emit AddedToWhitelist(_addr);
+        blacklist[_addr] = true;
+        emit AddedToBlacklist(_addr);
     }
 
     /**
@@ -135,16 +136,16 @@ contract KYCController is IController, KYCVerifierRole {
     * @dev Only addresses with the role @KYCVerifier are allowed to use this Function
     */
     function removeAddressFromWhitelist(address _addr) external onlyKYCVerifier {
-        whitelist[_addr] = false;
-        emit RemovedFromWhitelist(_addr);
+        blacklist[_addr] = false;
+        emit RemovedFromBlacklist(_addr);
     }
 
     /**
     * @notice Check if an address is present on the whitelist
     * @return returns true if the address is on the whitelist
     */
-    function _onWhitelist(address _addr) public view returns(bool) {
-        return (whitelist[_addr]); //TODO make whitelist expirable and check here?
+    function _onBlacklist(address _addr) public view returns(bool) {
+        return (blacklist[_addr]);
     }
 
 }
---
+++ contracts/Roles/InsiderListManagerRole.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+
+// A role for the global Verifiers for KYC information, who are allowed to enter
+contract InsiderListManagerRole {
+    using Roles for Roles.Role;
+
+    event InsiderListManagerAdded(address indexed account);
+    event InsiderListManagerRemoved(address indexed account);
+
+    Roles.Role private _insiderListManagers;
+
+    constructor () internal {
+        _addInsiderListManager(msg.sender);
+    }
+
+    modifier onlyInsiderListManager() {
+        require(isInsiderListManager(msg.sender), "InsiderListManagerRole: caller does not have the InsiderListManager role");
+        _;
+    }
+
+    function isInsiderListManager(address account) public view returns (bool) {
+        return _insiderListManagers.has(account);
+    }
+
+    function addInsiderListManager(address account) public onlyInsiderListManager {
+        _addInsiderListManager(account);
+    }
+
+    function renounceInsiderListManager() public {
+        _removeInsiderListManager(msg.sender);
+    }
+
+    function _addInsiderListManager(address account) internal {
+        _insiderListManagers.add(account);
+        emit InsiderListManagerAdded(account);
+    }
+
+    function _removeInsiderListManager(address account) internal {
+        _insiderListManagers.remove(account);
+        emit InsiderListManagerRemoved(account);
+    }
+}
