eedacddaff1cc9eec0520d823bcdc9a598f5b5d0
Johannes Brottrager <e1326320@student.tuwien.ac.at>
2019-06-02 22:39:34 +0200

    Switch Votingtokens to separate arrays for options due to deficiencies in the solidity language

--- contracts/Tokens/VotingToken.sol
+++ contracts/Tokens/VotingToken.sol
@@ -12,31 +12,33 @@ import "./DividendToken.sol";
 contract VotingToken is DividendToken{
 
 
+    event BallotCreated(
+        bytes32 ballotName
+    );
 
     /// @dev `Checkpoint` is the structure that attaches a block number to a
     ///  given value, the block number attached is the one that last changed the
     ///  value
-    struct  Checkpoint {
-
+    struct Checkpoint {
         // `fromBlock` is the block number that the value was generated super.mint(_to, _amount); from
         uint128 fromBlock;
-
         // `value` is the amount of tokens at a specific block number
         uint128 value;
     }
 
     // an option is a single possible result for a ballot that can be voted for
-    struct Option{
-        bytes32 name;
-        uint voteCount;
-    }
+//    struct Option{
+//        bytes32 name;
+//        uint voteCount;
+//    }
 
     // a ballot is a structure that corresponds to a single descision that can be
     // voted upon by token holders, each with a weight corresponding to their
     // owned tokens at the cutoff date
     struct Ballot{
         bytes32 name; // name of the current ballot / asked question
-        Option[] options; //list of all voteable options
+        bytes32[] optionNames; //list of all voteable options  //TODO check if these are public and can be changed
+        uint[] optionVoteCounts; //list of all resp. vote counts
         mapping (address => bool) voted; // record on which addresses already voted
         uint cutoffBlockNumber; // block number of the block, where the balances are counted for voting weight
     }
@@ -54,23 +56,13 @@ contract VotingToken is DividendToken{
     // Tracks the history of the `totalSupply` of the token
     Checkpoint[] totalSupplyHistory;
 
-    // TODO temporary storage of Options while creating a new Ballot
-    Option[] tempOptions;
-    Ballot tempBallot;
-
     // all ballots that can be voted upon by token holders
-    Ballot[]  ballots;
-    //TODO maybe make it a list of keys (names), + a map of ballots
+    Ballot[] public ballots;
 
     constructor(KYCController _kycController, InsiderListController _insiderListController, PEPListController _pepListController) DividendToken( _kycController,  _insiderListController, _pepListController) public { //The super contract is a modifier of sorts of the constructor
 
     }
 
-
-    //we dont need issue, because the underlying function _mint is edited
-    /*function issue(address _tokenHolder, uint _value, bytes memory _data) public onlyIssuer {
-        generateTokens(_to, _amount);
-    }*/
 
 
     ///////////////////
@@ -119,36 +111,6 @@ contract VotingToken is DividendToken{
         return balanceOfAt(_owner, block.number);
     }
 
-    /// @notice `msg.sender` approves `_spender` to spend `_amount` tokens on
-    ///  its behalf. This is a modified version of the ERC20 approve function
-    ///  to be a little bit safer
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _amount The amount of tokens to be approved for transfer
-    /// @return True if the approval was successful
- /*   function approve(address _spender, uint256 _amount) public returns (bool success) {
-
-        // To change the approve amount you first have to reduce the addresses`
-        //  allowance to zero by calling `approve(_spender,0)` if it is not
-        //  already 0 to mitigate the race condition described here:
-        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        require((_amount == 0) || (allowed[msg.sender][_spender] == 0));
-
-
-
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-        return true;
-    }*/
-
-    /// @dev This function makes it easy to read the `allowed[]` map
-    /// @param _owner The address of the account that owns the token
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens of _owner that _spender is allowed
-    ///  to spend
-/*    function allowance(address _owner, address _spender
-    ) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }*/
 
 
     ////////////////
@@ -159,38 +121,18 @@ contract VotingToken is DividendToken{
     /// @dev creates a new ballot and appends it to 'ballots'
     /// @param ballotName The name of the ballot resp. the asked Question
     /// @param optionNames List of possible choices / answers to the question
-    function createBallot(bytes32 ballotName, bytes32[] memory optionNames) public {
-        //The memory keyword is important here, or else the Ballot would be
-        //made a storage variable, as structs are as a standard
-        // https://solidity.readthedocs.io/en/latest/types.html#reference-types
-//        Ballot storage ballot;
-//
-//        ballot.name = ballotName;
-//        ballot.cutoffBlockNumber = block.number; //set the current Blocknumber as cutoff, the current balance distribution is stored
-        //TODO require name not empty and optionNames notEmpty
+    function createBallot(bytes32 ballotName, bytes32[] memory optionNames) public { //TODO only votingofficial
 
-       // Option[] storage options;// = bytes32[optionNames.length];
+        //Check the arguments for validity
+        require(ballotName[0] != 0, "The ballotName must not be empty!");
+        require(optionNames.length > 0, "The optionNames Array must not be empty!");
 
-        Ballot memory ballot = Ballot({name: ballotName, cutoffBlockNumber: block.number, options: new Option[](0)});
+
+        Ballot memory ballot = Ballot({name: ballotName, cutoffBlockNumber: block.number, optionNames: optionNames, optionVoteCounts: new uint[](optionNames.length)});
         ballots.push(ballot);
         //ballots[ballots.length-1].players.push(msg.sender);
 
-
-        delete tempOptions;
-
-
-        // add all optionNames from memory to the storage struct
-        for (uint i = 0; i < optionNames.length; i++) {
-            ballots[ballots.length-1].options.push(Option({ //TODO maybe split options in two
-            name: optionNames[i],
-            voteCount: 0
-            }));
-        }
-
-        //tempBallot = Ballot({name: ballotName, cutoffBlockNumber: block.number, options: tempOptions});
-
-        //ballots.push(Ballot({name: ballotName, cutoffBlockNumber: block.number, options: tempOptions}));
-//TODO event
+        emit BallotCreated(ballotName);
     }
 
     /// @info casts votes equal to the senders tokens at the cutoff time of the newest ballot designated
@@ -198,8 +140,7 @@ contract VotingToken is DividendToken{
     /// @ballotName Ballot to vote in
     /// @optionName option to vote for
     /// @dev rolls back on unfound ballot or option, if sender already voted, or does not own tokens at cutoff
-    function vote(bytes32 ballotName, bytes32 optionName) public { //TODO IF THIS DOES NOT WORK, try storage
-        //Ballot storage tempballot; //TODO may be the changes to this ballot object are not persistet
+    function vote(bytes32 ballotName, bytes32 optionName) public {
         //Search through all Ballots backwards, so that the recent ones are found first
         int found = -1;
 
@@ -222,10 +163,10 @@ contract VotingToken is DividendToken{
 
         //Search for the voted option in the ballot
 
-        for(int j = UIntConverterLib.toIntSafe(ballot.options.length); j>0; j--){
-            if(ballot.options[SafeMathInt.toUintSafe(j-1)].name==optionName){
+        for(int j = UIntConverterLib.toIntSafe(ballot.optionNames.length); j>0; j--){
+            if(ballot.optionNames[SafeMathInt.toUintSafe(j-1)]==optionName){
                 ballot.voted[msg.sender]=true;
-                ballot.options[SafeMathInt.toUintSafe(j-1)].voteCount+=senderBalance;  //TODO replace with safe functions
+                ballot.optionVoteCounts[SafeMathInt.toUintSafe(j-1)]+=senderBalance;  //TODO replace with safe functions
                 return;
             }
         }
@@ -252,22 +193,19 @@ contract VotingToken is DividendToken{
 
         uint winningVoteCount = 0;
         int winningOptionIndex = -1;
-        for (uint p = 0; p < ballot.options.length; p++) {
-            if (ballot.options[p].voteCount > winningVoteCount) {
-                winningVoteCount = ballot.options[p].voteCount;
+        for (uint p = 0; p < ballot.optionVoteCounts.length; p++) {
+            if (ballot.optionVoteCounts[p] > winningVoteCount) {
+                winningVoteCount = ballot.optionVoteCounts[p];
                 winningOptionIndex = UIntConverterLib.toIntSafe(p);
             }
         }
         require(winningOptionIndex !=-1,"No votes yet!");
 
 
-        winningOptionName=ballot.options[SafeMathInt.toUintSafe(winningOptionIndex)].name;
-        winningOptionVoteCount=ballot.options[SafeMathInt.toUintSafe(winningOptionIndex)].voteCount;
+        winningOptionName=ballot.optionNames[SafeMathInt.toUintSafe(winningOptionIndex)];
+        winningOptionVoteCount=ballot.optionVoteCounts[SafeMathInt.toUintSafe(winningOptionIndex)];
     }
 
-
-
-    //TODO!! get ballot methode!!!!!!!!!
 
 
     ////////////////
@@ -303,7 +241,7 @@ contract VotingToken is DividendToken{
     /// @param _owner The address that will be assigned the new tokens
     /// @param _value The quantity of tokens generated
     /// @return True if the tokens are generated correctly
-    function  _mint(address _account, uint256 _value) internal { //TODO check if it overwrites although variable names differ
+    function  _mint(address _account, uint256 _value) internal {
         require(_account != address(0));
         //TODO use checks and safemath as it is used in other token contracts
         uint curTotalSupply = totalSupply();
@@ -317,25 +255,28 @@ contract VotingToken is DividendToken{
 
 
     /// @notice Burns `_amount` tokens from `_owner`
+    /// @dev overwrites the erc20 implementation, is also used by _burnFrom
     /// @param _owner The address that will lose the tokens
     /// @param _amount The quantity of tokens to burn
     /// @return True if the tokens are burned correctly
-    function _burn(address _account, uint _value  //TODO burnfrom, todo unedited
+    function _burn(address _account, uint _value
     )  internal  {
+        //erc 20 checks
+        require(_account != address(0));
+        require(_value <= balanceOf(_account));
+
         uint curTotalSupply = totalSupply();
         require(curTotalSupply >= _value);
         uint previousBalanceFrom = balanceOf(_account);
         require(previousBalanceFrom >= _value);
-        updateValueAtNow(totalSupplyHistory, curTotalSupply - _value);
-        updateValueAtNow(_balances[_account], previousBalanceFrom - _value);
-        emit Transfer(address(0), _account, _value);
+        updateValueAtNow(totalSupplyHistory, curTotalSupply.sub(_value));
+        updateValueAtNow(_balances[_account], previousBalanceFrom.sub(_value));
+        emit Transfer(_account,address(0), _value);
     }
-
 
     /// @dev This function makes it easy to get the total number of tokens
     /// @return The total number of tokens
     function totalSupply() public view returns (uint) {
-
         return totalSupplyAt(block.number);
     }
 
@@ -360,7 +301,7 @@ contract VotingToken is DividendToken{
         uint min = 0;
         uint max = checkpoints.length-1;
         while (max > min) {
-            uint mid = (max + min + 1)/ 2;
+            uint mid = (max.add(min).add(1))/ 2;
             if (checkpoints[mid].fromBlock<=_block) {
                 min = mid;
             } else {
@@ -386,34 +327,5 @@ contract VotingToken is DividendToken{
             oldCheckPoint.value = uint128(_value);
         }
     }
-
-
-    /// @notice The fallback function: If the contract's controller has not been
-    ///  set to 0, then the `proxyPayment` method is called which relays the
-    ///  ether and creates tokens as described in the token controller contract
-  /*  function () external payable {
-
-    }
-*/
-    //@dev utility string search function
-    //@array array to search
-    //@searchstring string to search for in array
-    //@return first index if found, -1 if not found
-    /*function searchStringArrayBackwards(string[] array, string searchstring) private returns (int) {
-        for(uint i = ballots.length; i>0; i--){
-            if(compareStrings(ballots[i-1],ballotName)){
-                return i-1;
-            }
-        }
-        return -1;
-    }
-*/
-
-    //@dev utility string comparison function TODO doc
-   /* function compareStrings (bytes32 a, bytes32 b) public view
-        returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
-
-    }*/
 
 }
